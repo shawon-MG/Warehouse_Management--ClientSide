3dc08867b0e0ead80cf9cde91640c0aea1be48de
shawon-MG <work.shawomg@gmail.com>
2022-05-01 23:35:58 +0600

    Project Initalized with adding nav bar

---
+++ src/components/Main/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Main = () => {
+    return (
+        <div className='mb-4'>
+
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Nav className="mx-auto p-2">
+                        <Navbar.Brand className='me-5' href="#home">Manage Item</Navbar.Brand>
+                        <Navbar.Brand className='me-5' href="#home">Add Item</Navbar.Brand>
+                        <Navbar.Brand className='me-5' href="#home">My Item</Navbar.Brand>
+                        <Navbar.Brand className='me-5' href="#home">Sign In</Navbar.Brand>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Main;
