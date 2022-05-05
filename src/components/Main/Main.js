c812f99be29b561358acd68736c3e81c43810b75
shawon-MG <work.shawomg@gmail.com>
2022-05-05 14:11:12 +0600

    reload issue fixed

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,87 +1,46 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-// import app from '../../firebase.config';
-// import { useState } from 'react';
+
 
 import useFirebase from '../../hooks/useFirebase';
 
 
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
-
-// const auth = getAuth(app);
 
 const Main = () => {
 
-    // const [user, setUser] = useState({});
-    // const googleProvider = new GoogleAuthProvider();
-
-    // const handleGoogleSignIn = () => {
-
-    //     signInWithPopup(auth, googleProvider)
-    //         .then(result => {
-    //             const user = result.user;
-    //             setUser(user);
-    //             console.log(user);
-    //         })
-    //         .catch(error => {
-    //             console.log('Error', error);
-    //         })
-    // };
     const { user, handleSignOut } = useFirebase();
 
     return (
-        <div className='mb-4'>
 
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">Electronic Items Warehouse </Navbar.Brand>
-                    <Nav className="mx-auto p-2">
+        <nav class="navbar navbar-dark bg-dark mb-2 mt-0 rounded d-flex justify-content-center">
+            <Link className='pe-5 text-white fs-3 text-decoration-none' to='/'> Electronic Items Warehouse</Link>
+            <Link className='pe-4 text-white fs-5 text-decoration-none' to='/manage-item'> manage-item</Link>
+            <Link className='pe-4 text-white fs-5 text-decoration-none' to='/add-item'> add-item</Link>
+            <Link className='pe-4 text-white fs-5 text-decoration-none' to='/my-item'> my-item</Link>
+            <Link className='pe-4 text-white fs-5 text-decoration-none' to='/blogs'> blogs</Link>
 
-
-
-
-                        <Navbar.Brand className='me-4' href="/manage-item">Manage Item</Navbar.Brand>
-
-                        <Navbar.Brand className='me-4' href="/add-item">Add Item</Navbar.Brand>
-
-                        <Navbar.Brand className='me-4' href="/my-item">My Item</Navbar.Brand>
-
-                        <Navbar.Brand className='me-4' href="/blogs">Blogs</Navbar.Brand>
+            {
+                user?.uid
+                    ?
+                    <Button onClick={handleSignOut} variant="secondary" className='text-white me-3'> Sign Out</Button>
+                    :
+                    <Link className='bg-secondary text-white p-2 rounded fs-5 text-decoration-none' to='/signIn'> Sign In</Link>
 
 
+            }
+            {
+                user?.uid
+                    ?
+                    <h5 className='text-white mt-2 ps-2'> Hi, <span className='bg-white text-dark p-1 me-1 rounded'> {user.displayName} </span></h5>
+                    :
+                    <h5 className='text-white mt-2 ps-2'> Please, sign in</h5>
 
-                        {
-                            user?.uid
-                                ?
-                                <Button onClick={handleSignOut} variant="secondary" className='text-white me-3' >Sign Out</Button>
-                                :
-                                <Button href="/signIn" variant="secondary" className='text-white me-3' >Sign In</Button>
-
-
-                        }
-
-
-
-                        {/* <Button variant="secondary" className='text-white me-3' onClick={handleGoogleSignIn}>Google</Button> */}
+            }
+        </nav>
 
 
-
-                        {
-                            user?.uid
-                                ?
-                                <h5 className='text-white mt-2 ps-2'> Hi, <span className='bg-white text-dark p-1 me-1 rounded'> {user.displayName} </span></h5>
-                                :
-                                <h5 className='text-white mt-2 ps-2'> Please, sign in</h5>
-
-                        }
-
-                    </Nav>
-                </Container>
-            </Navbar>
-
-        </div >
     );
 };
 
