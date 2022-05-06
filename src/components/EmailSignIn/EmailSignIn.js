7294ccdc740f513c5310466ae86e166e99bd1314
shawon-MG <work.shawomg@gmail.com>
2022-05-06 19:37:41 +0600

    sign up, sign in btn added

--- src/components/EmailSignIn/EmailSignIn.js
+++ src/components/EmailSignIn/EmailSignIn.js
@@ -1,125 +1,59 @@
-// import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
-// import { useState } from 'react';
-// import { Button, Form } from 'react-bootstrap';
-// import { useLocation, useNavigate } from 'react-router-dom';
-// import { auth } from '../../firebase.config';
-// import useFirebase from '../../hooks/useFirebase';
-
-// const SignIn = () => {
-
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [register, setRegister] = useState(false);
-
-//     const handleEmailBlur = (event) => {
-//         setEmail(event.target.value);
-//     }
-//     const handlePasswordBlur = (event) => {
-//         setPassword(event.target.value);
-//     }
-
-//     const handleRegistration = event => {
-//         setRegister(event.target.checked);
-//     }
-
-//     const emailVarification = () => {
-//         sendEmailVerification(auth.currentUser)
-//             .then(() => {
-//                 console.log('Email Sent');
-//             })
-//     }
-
-//     const handleSubmit = (event) => {
-//         if (register) {
-//             signInWithEmailAndPassword(auth, email, password)
-//                 .then(result => {
-//                     const user = result.user;
-//                     console.log(user);
-//                 })
-//                 .catch(error => {
-//                     console.error(error);
-//                 })
-//         }
-//         else {
-//             createUserWithEmailAndPassword(auth, email, password)
-//                 .then(result => {
-//                     const user = result.user;
-//                     console.log(user);
-//                     emailVarification();
-//                 })
-//                 .catch(error => {
-//                     console.error(error);
-//                 })
-//         }
-//         console.log('SUBMITTED: ', email, password);
-//         event.preventDefault();
-//     }
-
-//     const handlePasswordReset = () => {
-//         sendPasswordResetEmail(auth, email)
-//             .then(() => {
-//                 console.log('Email Sent');
-//             })
-//     };
-
-//     const { user, handleGoogleSignIn } = useFirebase();
-
-//     const navigate = useNavigate();
-//     const location = useLocation();
-//     const from = location?.search?.from?.pathname || '/';
-
-//     if (user) {
-//         navigate(from, { replace: true });
-//     }
-
-//     const googleSignIn = () => {
-//         handleGoogleSignIn()
-//             .then(() => {
-//                 navigate(from, { replace: true })
-//             })
-//     };
-
-//     return (
-//         <div>
-//             <div className="registration w-50 mx-auto">
-//                 <h2 className='bg-secondary text-white p-2 mt-4'> Please {register ? 'Log-In' : 'Register'}</h2>
-//                 <Form onSubmit={handleSubmit}>
-//                     <Form.Group className="mb-3" controlId="formBasicEmail">
-//                         <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter Your Email" required />
-//                     </Form.Group>
-
-//                     <Form.Group className="mb-3" controlId="formBasicPassword">
-//                         <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Enter Your Password" required />
-//                     </Form.Group>
-//                     <Form.Group className="mb-3 " controlId="formBasicCheckbox">
-//                         <Form.Check onChange={handleRegistration} type="checkbox" label="Registered Already ?" />
-//                     </Form.Group>
-//                     <Button variant="secondary" type="submit" className='text-white'>
-//                         {register ? 'Log-In' : 'Register'}
-//                     </Button> <br />
-//                     <Button onClick={handlePasswordReset} variant="link" className='text-dark'>Forget Password?</Button>
-//                 </Form>
-//             </div>
-
-//             <Button onClick={googleSignIn} variant="secondary" size="lg" className="mt-4">
-//                 Google Sign In
-//             </Button>
-
-//         </div>
-//     );
-// };
-// export default SignIn;
-
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { auth } from '../../firebase.config';
+import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase.config';
 
-const EmailSignIn = () => {
+const SignIn = () => {
+
+    const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+    const [sendEmailVerification] = useSendEmailVerification(auth);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [register, setRegister] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmailBlur = e => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordBlur = e => {
+        setPassword(e.target.value);
+    };
+    const handleRegistration = event => {
+        setRegister(event.target.checked);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (register) {
+            if (password.length < 6) {
+                setError('Password must be more than 6 characters')
+                return;
+            }
+            signInWithEmailAndPassword(email, password)
+                .then(() => {
+                    navigate(from, { replace: true })
+                });
+        }
+        else {
+            if (password.length < 6) {
+                setError('Password Must Be More Than 6 Characters')
+                return;
+            }
+            createUserWithEmailAndPassword(email, password)
+                .then(() => {
+                    navigate(from, { replace: true })
+                });
+        }
+        await sendEmailVerification();
+        Window.alert('Varify Your Email Address');
+
+    };
 
     const [signInWithGoogle] = useSignInWithGoogle(auth);
-
     const navigate = useNavigate();
     const location = useLocation();
     const from = location?.state?.from?.pathname || '/';
@@ -128,15 +62,133 @@ const EmailSignIn = () => {
             .then(() => {
                 navigate(from, { replace: true })
             })
-    }
+    };
+
     return (
+
         <div>
-            <h2>Log In</h2>
+            <div className="registration w-50 mx-auto">
+                <h2 className='bg-secondary text-white p-2 mt-4'> Please {register ? 'Sign In' : 'Register'}</h2>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter Your Email" required />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Enter Your Password" required />
+                    </Form.Group>
+
+                    <p className='text-danger'><strong>{error}</strong></p>
+
+                    <Form.Group className="mb-3 " controlId="formBasicCheckbox">
+                        <Form.Check onChange={handleRegistration} type="checkbox" label="Registered Already ?" />
+                    </Form.Group>
+
+                    <Button variant="secondary" type="submit" className='text-white'>
+                        {register ? 'Sign In' : 'Register'}
+                    </Button> <br />
+                </Form>
+            </div>
+
             <Button onClick={handleGoogleSignIn} variant="secondary" size="lg" className="mt-4">
                 Google Sign In
             </Button>
+
         </div>
     );
 };
 
-export default EmailSignIn;
+export default SignIn;
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { Button, Form } from 'react-bootstrap';
+// import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+// import { auth } from '../../firebase.config';
+// import { useLocation, useNavigate } from 'react-router-dom';
+
+// const EmailSignIn = () => {
+
+//     const [signInWithGoogle] = useSignInWithGoogle(auth);
+//     const navigate = useNavigate();
+//     const location = useLocation();
+//     const from = location?.state?.from?.pathname || '/';
+//     const handleGoogleSignIn = () => {
+//         signInWithGoogle()
+//             .then(() => {
+//                 navigate(from, { replace: true })
+//             })
+//     };
+
+//     const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [register, setRegister] = useState('');
+//     const [error, setError] = useState('');
+
+//     const handleEmailBlur = e => {
+//         setEmail(e.target.value);
+//     };
+//     const handlePasswordBlur = e => {
+//         setPassword(e.target.value);
+//     };
+//     const handleCreateAccount = e => {
+//         e.preventDefault();
+//         if (password.length < 6) {
+//             setError('Password must be more than 6 characters')
+//             return;
+//         }
+//         createUserWithEmailAndPassword(email, password)
+//             .then(() => {
+//                 navigate(from, { replace: true })
+//             });
+//     };
+
+//     return (
+//         <div>
+//             {/* <div className="registration w-50 mx-auto">
+//                 <h2 className='bg-secondary text-white p-2 mt-4'> Please Register First</h2>
+//                 <Form >
+//                     <Form.Group onBlur={handleEmailBlur} className="mb-3" controlId="formBasicEmail">
+//                         <Form.Control type="email" placeholder="Enter Your Email" required />
+//                     </Form.Group>
+
+//                     <Form.Group onBlur={handlePasswordBlur} className="mb-3" controlId="formBasicPassword">
+//                         <Form.Control type="password" placeholder="Enter Your Password" required />
+//                     </Form.Group>
+
+//                     <p className='text-danger'> {error} </p>
+
+//                     <Form.Group className="mb-3 " controlId="formBasicCheckbox">
+//                         <Form.Check type="checkbox" label="Registered Already ?" />
+//                     </Form.Group>
+//                     <Button onSubmit={handleCreateAccount} variant="secondary" type="submit" className='text-white'>
+//                         Register
+//                     </Button> <br />
+//                     <Button variant="link" className='text-dark'>Forget Password?</Button>
+//                 </Form>
+//             </div> */}
+
+//             <div>
+//                 <form>
+//                     <input onBlur={handleEmailBlur} type="email" name="" id="" /> <br /> <br />
+//                     <input onBlur={handlePasswordBlur} type="password" name="" id="" /><br /> <br />
+//                     <input onSubmit={handleCreateAccount} type="submit" value="submit" />
+//                 </form>
+//             </div>
+
+
+//             <Button onClick={handleGoogleSignIn} variant="secondary" size="lg" className="mt-4">
+//                 Google Sign In
+//             </Button>
+//         </div>
+//     );
+// };
+
+// export default EmailSignIn;
