83648b0707943c91ef840978de3d4ea8aaabc90f
shawon-MG <work.shawomg@gmail.com>
2022-05-08 15:13:20 +0600

    extra two section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel, CarouselItem } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import useLoadeddata from '../../hooks/useLoadeddata';
 import Item from '../Item/Item';
@@ -10,6 +11,9 @@ const Home = () => {
     console.log(items);
 
     const navigate = useNavigate();
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => console.log(data);
 
     return (
         <div>
@@ -53,29 +57,45 @@ const Home = () => {
                 <button onClick={() => navigate('/manage-item')} type="button" className="btn btn-link btn-lg bg-dark text-white mt-2 m-4">Manage Items</button>
             </div>
 
-            <Carousel >
-                <CarouselItem>
-                    <img className="rounded" width={900} height={500} alt="900x500" src="https://i.ibb.co/1Zw1Z9f/handsome-young-businessman-shirt-eyeglasses-85574-6228.webp" />
-                    <div className="carousel-caption bg-dark text-white w-50 mx-auto rounded">
-                        <h2>Andru Smith</h2>
-                        <p> <strong>I found this electronics warehouse has the best customer service team.</strong> </p>
-                    </div>
-                </CarouselItem>
-                <CarouselItem>
-                    <img className="rounded" width={900} height={500} alt="900x500" src="https://i.ibb.co/hfBPDLc/attractive-guy-shirt-isolated-white-background-185193-73693.webp" />
-                    <div className="carousel-caption bg-dark text-white w-50 mx-auto rounded">
-                        <h2>Tomas Willman</h2>
-                        <p><strong>They provide the best cheap options available in the market</strong></p>
-                    </div>
-                </CarouselItem>
-                <CarouselItem>
-                    <img className="rounded" width={900} height={500} alt="900x500" src="https://i.ibb.co/ky4MGns/yes-very-good-black-guy-smiling-showing-thumb-up-approval-like-agree-african-american-man-praise-goo.webp" />
-                    <div className="carousel-caption bg-dark text-white w-50 mx-auto rounded">
-                        <h2>William Burn</h2>
-                        <p><strong> Various type of products here with best brands in the market.</strong></p>
-                    </div>
-                </CarouselItem>
-            </Carousel>
+            <div className='p-4 w-75 mx-auto shadow-lg  rounded'>
+                <h2 className='bg-secondary text-white w-25 mx-auto p-1 rounded'> Customer Review </h2>
+                <Carousel>
+                    <CarouselItem>
+                        <img className="rounded" width={900} height={500} alt="900x500" src="https://i.ibb.co/1Zw1Z9f/handsome-young-businessman-shirt-eyeglasses-85574-6228.webp" />
+                        <div className="carousel-caption bg-dark text-white w-50 mx-auto rounded">
+                            <h2>Andru Smith</h2>
+                            <p> <strong>I found this electronics warehouse has the best customer service team.</strong> </p>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                        <img className="rounded" width={900} height={500} alt="900x500" src="https://i.ibb.co/hfBPDLc/attractive-guy-shirt-isolated-white-background-185193-73693.webp" />
+                        <div className="carousel-caption bg-dark text-white w-50 mx-auto rounded">
+                            <h2>Tomas Willman</h2>
+                            <p> <strong>They provide the best cheap options available in the market.</strong> </p>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                        <img className="rounded" width={900} height={500} alt="900x500" src="https://i.ibb.co/ky4MGns/yes-very-good-black-guy-smiling-showing-thumb-up-approval-like-agree-african-american-man-praise-goo.webp" />
+                        <div className="carousel-caption bg-dark text-white w-50 mx-auto rounded">
+                            <h2>William Burn</h2>
+                            <p> <strong> Various type of products here with best brands in the market.</strong> </p>
+                        </div>
+                    </CarouselItem>
+                </Carousel>
+            </div>
+
+            <div className='p-2 m-4'>
+                <h2 className='bg-secondary text-white w-25 mx-auto p-1 mt-4 rounded'>Contact With US</h2>
+                <div className='w-75 mx-auto rounded shadow-lg p-4'>
+                    < form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                        <input className='mb-4' placeholder='Your Name'  {...register("name")} />
+                        <input className='mb-4' placeholder='Email'  {...register("email")} />
+                        <textarea className='mb-4' placeholder='Your Massage'  {...register("massage")} />
+
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div >
+            </div>
 
 
             <div className='bg-dark mt-4 p-4 rounded'>
